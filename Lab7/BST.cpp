2947bee355a955785257f282f73733452cef5cf7
J.P. Shavin Anjitha Chandrawansha <shavinanjithachandrawansha@gmail.com>
2023-04-27 15:37:19 +0530

    update the lab 7

--- Lab7/BST.cpp
+++ Lab7/BST.cpp
@@ -19,21 +19,23 @@ void traverseInOrder(struct node *root) {
 
 // Insert a node
 struct node *insertNode(struct node *node, int key) {
-    cout << "enter";
+    // cout << "enter";
     if (node == nullptr) {
-        struct node *newNode;
-        *newNode = {.key = key, .left = nullptr, .right = nullptr};
-        return newNode;
+        node = new struct node;
+        node->key = key;
+        return node;
     }
-    cout << "root pass";
+    // cout << "root pass";
     if (key > node->key) {
         // insert the new node recursively based on the existance of the right
         if (node->right != nullptr) 
             return insertNode(node->right, key);
         else {
-            struct node *newNode;
-            *newNode = {.key = key, .left = nullptr, .right = nullptr};
-            node->right = newNode;
+            // struct node *newNode;
+            // *newNode = {.key = key, .left = nullptr, .right = nullptr};
+            node->right = new struct node;
+            node->right->key = key;
+            cout << "insert " << key << endl;
             return node;
         }
     } else {
@@ -42,10 +44,12 @@ struct node *insertNode(struct node *node, int key) {
         if (node->left != nullptr) 
             return insertNode(node->left, key);
         else {
-            cout << "insert";
-            struct node *newNode;
-            *newNode = {.key = key, .left = nullptr, .right = nullptr};
-            node->left = newNode;
+            cout << "else";
+            // struct node *newNode;
+            // *newNode = {.key = key, .left = nullptr, .right = nullptr};
+            node->left = new struct node;
+            node->left->key = key;
+            cout << "insert " << key << endl;
             return node;
         }
     }
@@ -72,10 +76,10 @@ void replant(struct node* root, struct node* current, struct node* parent, struc
 struct node *deleteNode(struct node *root, int key) {
 
     // find the node which needed to be deleted
-    struct node *currentNode{root}, *prevNode{nullptr};
+    struct node *currentNode{root}, *parent{nullptr};
     
     while (currentNode->key != key && currentNode != nullptr) {
-        prevNode = currentNode;
+        parent = currentNode;
         if (key > currentNode->key) {
             currentNode = currentNode->right;
         } else {
@@ -88,6 +92,30 @@ struct node *deleteNode(struct node *root, int key) {
     if (currentNode != nullptr) {
 
         if (currentNode->left == nullptr && currentNode->right == nullptr) {
+            replant(root, currentNode, parent, nullptr);
+        } else if (currentNode->left == nullptr) {
+            replant(root, currentNode, parent, currentNode->right);
+        } else if (currentNode->right == nullptr) {
+            replant(root, currentNode, parent, currentNode->left);
+        } else {
+
+            struct node *succ{currentNode->right}, *succParent{nullptr};
+            while (succ->left != nullptr) {
+                succParent = succ;
+                succ = succ->left;
+            }
+
+            if (succ == currentNode->right) {
+                currentNode->key = succ->key;
+                currentNode->right = succ->right;
+                delete succ;
+            } else {
+
+                int k = succ->key;
+                replant(root, succ, succParent, succ->right);
+                currentNode->key = k;
+
+            }
 
         }
 
@@ -113,7 +141,6 @@ int main() {
         cin >> operand;
         root = insertNode(root, operand);
         cin >> operation;
-        cout << operation;
         break;
       case 2: // delete
         cin >> operand;
--- pointers/pointer1.cpp
+++ pointers/pointer1.cpp
@@ -11,7 +11,6 @@ int main() {
     int array[5]{1, 2, 3, 4, 5};
     int* arrPtr{&array[2]};
     arrPtr++;
-    cout << array[6] << endl;
     cout << "pointer size: " << sizeof(arrPtr) << endl;
 
     cout << "size of array: " << sizeof(array) << " , " << sizeof(int) << endl;
